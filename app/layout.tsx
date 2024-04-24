6796294f56677d410c8e9fe6decb94cadb6441e2
Your Name <crismailinpolancocemas@gmail.com>
2024-04-24 07:41:29 -0400

    feat: creation of blocks in the app and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,102 @@ export default function Page() {
 
         </div>
       </div>
+    
+    <main className="flex min-h-screen flex-col p-6">
+
+  <div className='row-span-1  grid-row: span 1 / span 1;  w-[100%] h-[100%] max-w-[500px] max-h-[500px] bg-gray-200	bg-["rgb(229 231 235")] p-4'>
+  <div className='w-full h-full flex justify-center items-center flex-col text-center text-xl'>
+    <h2 className='text-center text-2xl'>
+      <strong>Misión</strong> </h2><p>
+Nuestra misión es desarrollar y proporcionar herramientas tecnológicas avanzadas para optimizar la recolección de desechos sólidos, promoviendo entornos más limpios y sostenibles. Comprometidos con diseñar soluciones inteligentes para una gestión eficaz de los residuos.
+</p>
+</div> 
+</div>
+<br />
+
+  <div className='row-span-2  grid-row: span 2 / span 2; w-[100%] h-[100%] max-w-[500px] max-h-[500px] bg-gray-200	bg-["rgb(229 231 235")] p-4'>
+    <div className='w-full h-full flex justify-center items-center flex-col text-center text-xl'>
+    <h2 className='text-center text-2xl'>
+      <strong>Visión:</strong> </h2><p>
+Convertirnos en líderes en el desarrollo y comercialización de tecnologías para la gestión y reciclaje de residuos, marcando el estándar de la industria y contribuyendo significativamente a la preservación del medio ambiente global.
+</p>
+</div>
+</div>
+<br />
+  <div className='row-span-3  grid-row: span 3 / span 3; w-[100%] h-[100%] max-w-[500px] max-h-[500px]  bg-gray-200	bg-["rgb(229 231 235")] p-4'>
+  <div className='w-full h-full flex justify-center items-center flex-col text-center text-xl'>
+    <h2 className='text-center text-2xl'>
+    <strong>Valores</strong></h2><p>
+    <ul>
+      <li><strong>Innovación:</strong> Buscamos constantemente nuevas tecnologías y soluciones para abordar los desafíos de la gestión de residuos.</li><br />
+
+      <li><strong>Sostenibilidad:</strong> Nos comprometemos a promover prácticas que cuiden el medio ambiente y apoyen la sostenibilidad.</li><br />
+
+      <li><strong>Eficiencia:</strong> Desarrollamos herramientas que optimicen la recolección y gestión de desechos, maximizando la eficiencia.</li> <br />
+
+      <li><strong>Colaboración:</strong> Fomentamos alianzas estratégicas y colaboraciones para lograr un impacto más amplio y significativo en nuestra misión.</li> <br />
+    </ul>
+</p>
+</div>
+<br />
+</div>
+<br />
+  <div className='row-span-4  grid-row: span 4 / span 4; w-[100%] h-[100%] max-w-[500px] max-h-[500px] bg-gray-200	bg-["rgb(229 231 235")] p-4'>
+  <div className='w-full h-full flex justify-center items-center flex-col text-center text-xl'>
+    <h2 className='text-center text-2xl'>
+      <strong>Objetivo general</strong> </h2><p>
+Dar a conocer y posicionar a “Green Tech” como una empresa dedicada, que se comprometa a desarrollar y comercializar tecnologías para la gestión de residuos y el reciclaje de los mismos.
+</p></div>
+</div>
+<br />
+<div className='row-span-5  grid-row: span 5 / span 5; w-[100%] h-[100%] max-w-[500px] max-h-[500px] bg-gray-200	bg-["rgb(229 231 235")] p-4'>
+<div className='w-full h-full flex justify-center items-center flex-col text-center text-xl'>
+    <h2 className='text-center text-2xl'>
+  <strong>Objetivos específicos</strong></h2><p>
+  <ul>
+    <li>Desarrollar tecnologías innovadoras.</li>
+      <li>Establecer alianzas estratégicas.</li>
+      <li>Implementar pilotos exitosos.</li>
+
+  </ul>
+</p>
+</div>
+</div>
+<br />
+<div className='bg-[#34495E]'>
+  <div className='text-white  color: rgb(255 255 255);'>
+<header>
+    <h1><strong>Contacto</strong></h1>
+  </header>
+
+  <main>
+    <section className="text-center	text-align: center;">
+      <h2><strong>Información de contacto</strong></h2>
+      <p>Cutupú, La Vega</p>
+      <p>Plaza Flamingo</p>
+      <p>+1 829-754-6758</p>
+      <p><a href="mailto:support@company.com">greentech@company.com</a></p>
+    </section>
+
+    <section className="text-right	text-align: right;">
+      <h2><strong>Redes sociales</strong></h2>
+      <ul>
+        <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+      </ul>
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2024 Company Green Tech. Todos los derechos reservados.</p>
+  </footer>
+  
+  </div>
+  </div>
     </main>
+
+
+</main>
   );
 }
